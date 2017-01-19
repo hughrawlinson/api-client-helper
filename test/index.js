5ac0fccde96dd50007767263c19b1432bb4e41d8
Hugh Rawlinson <hugh@spotify.com>
2017-01-19 22:51:01 +0100

    Add spec for endpoints function

--- test/index.js
+++ test/index.js
@@ -3,19 +3,16 @@
 import test from 'ava';
 import endpoint from '../src/endpoint';
 
-test('happy ponies', () => {
-  const fetch = () => null;
-  api(null, null, {
-    baseUri: 'http://api.example.com/v1',
-    endpoints: [
-      {
-        endpoint: '/happy/ponies/{id}',
-        method: 'GET',
-        requiredParams: ['id'],
-        optionalParams: ['lastSeenId'],
-      }
-    ]
-  });
+test('endpoint returns correctly partially applied function', (t) => {
+  const endpointConfig = {
+    uri: 'http://example.com',
+  };
+  const init = () => {};
+  const Request = (uriOpt, initOpt) => {
+    t.true(uriOpt === endpointConfig.uri);
+    t.true(initOpt.toString() === init.toString());
+  };
+  endpoint(Request, init)(endpointConfig);
 });
 
 test.skip('returns a request object with the correct url', (t) => {
