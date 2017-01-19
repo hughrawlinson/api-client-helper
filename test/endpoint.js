026fe81f3321e3742c9e2c1890fedb64c4a0daf0
Hugh Rawlinson <hugh@spotify.com>
2017-01-20 00:03:54 +0100

    Re-add endpoint test
    It got lost during a refactor

---
+++ test/endpoint.js
@@ -0,0 +1,16 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint-env node, mocha */
+import test from 'ava';
+import endpoint from '../src/endpoint';
+
+test('endpoint returns correctly partially applied function', (t) => {
+  const endpointConfig = {
+    uri: 'http://example.com',
+  };
+  const init = () => {};
+  const Request = (uriOpt, initOpt) => {
+    t.true(uriOpt === endpointConfig.uri);
+    t.true(initOpt.toString() === init.toString());
+  };
+  endpoint(Request, init)(endpointConfig);
+});
