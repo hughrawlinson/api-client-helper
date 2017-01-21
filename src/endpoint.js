cf9a83178d83f5b37675094e1673e4ed1c4e4b7e
Hugh Rawlinson <hugh@spotify.com>
2017-01-21 12:34:50 +0100

    move object creation to endpoint method

--- src/endpoint.js
+++ src/endpoint.js
@@ -2,9 +2,11 @@ const endpoint = (requestProvider, init) => endpointConfig =>
   // this function should take a configuration and return a function that will
   // take and validate parameters to the configuration before returning a
   // Request object
-  params => requestProvider(
-    endpointConfig.uri,
-    init(params, endpointConfig)
-  );
+  params => ({
+    [endpointConfig.name]: requestProvider(
+      endpointConfig.uri,
+      init(params, endpointConfig)
+    ),
+  });
 
 module.exports = endpoint;
